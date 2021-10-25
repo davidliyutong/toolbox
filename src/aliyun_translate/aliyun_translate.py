5580138f67acc20ee802f914106520580587e153
davidliyutong <32391509+davidliyutong@users.noreply.github.com>
2021-10-25 10:10:52 +0800

    Add multiprocessing to aliyun_translate

--- src/aliyun_translate/aliyun_translate.py
+++ src/aliyun_translate/aliyun_translate.py
@@ -16,6 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 BUCKET_ENDPOINT = os.getenv('BUCKET_ENDPOINT')
 BUCKET_NAME = os.getenv('BUCKET_NAME')
 
+N_RETRY: int = 12
 
 def put_file_to_oss(bucket: oss2.Bucket, file_path: str, timeout: int = 120) -> Tuple[str, str]:
 
@@ -54,24 +55,9 @@ def get_translation_task(client, task_id: str) -> alimt_20181012_models.GetDocTr
     res = client.get_doc_translate_task(get_doc_translate_task_request)
     return res
 
-def main(args):
-    N_RETRY = 12
-
-    FILENAME = args.filename
-    SRC_LANG = args.src_lang
-    DST_LANG = args.dst_lang
-    auth = oss2.Auth(ACCESS_KEY, SECRET_KEY)
-    bucket = oss2.Bucket(auth, BUCKET_ENDPOINT, BUCKET_NAME)
-
-    config = open_api_models.Config(access_key_id=ACCESS_KEY, access_key_secret=SECRET_KEY)
-    config.endpoint = 'mt.aliyuncs.com'
-    client = alimt20181012Client(config)
-
-    # bucket, client is needed here
-
-
-    object_name, url = put_file_to_oss(bucket, FILENAME)
-    res = create_translation_task_from_url(client, url, SRC_LANG, DST_LANG)
+def process_file(bucket, client, filename, src_lang, dst_lang):
+    object_name, url = put_file_to_oss(bucket, filename)
+    res = create_translation_task_from_url(client, url, src_lang, dst_lang)
 
     if res.body.status == 'ready':
         task_id = res.body.task_id
@@ -93,6 +79,23 @@ def main(args):
         print(f"[ Error ] Failed to translate document Result: {res}")
 
     delete_object_from_oss(bucket, object_name)
+
+def main(args):
+
+    FILENAME: str = args.filename
+    SRC_LANG: str = args.src_lang
+    DST_LANG: str = args.dst_lang
+    auth = oss2.Auth(ACCESS_KEY, SECRET_KEY)
+    bucket = oss2.Bucket(auth, BUCKET_ENDPOINT, BUCKET_NAME)
+
+    config = open_api_models.Config(access_key_id=ACCESS_KEY, access_key_secret=SECRET_KEY)
+    config.endpoint = 'mt.aliyuncs.com'
+    client = alimt20181012Client(config)
+
+    # bucket, client is needed here
+    for filename in FILENAME.split(','):
+        process_file(bucket, client, filename, SRC_LANG, DST_LANG)
+        
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--src_lang", type=str, default="en")
